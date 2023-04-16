85b060c12120e0c264aa176a2ab2dc52d90da8bb
sderbidge <55034668+sderbidge@users.noreply.github.com>
2023-04-15 19:26:57 -0600

    feat: added content to violin and exp to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,52 @@ import React from "react";
 import { FaLinkedin, FaTwitter, FaFacebook, FaGithub } from "react-icons/fa";
 
 import "./Footer.css";
-export default function Footer() {
+import { useState } from "react";
+export default function Footer({ addExp, addExpNotif }) {
+  const [linkedinExp, setLinkedInExp] = useState(1)
+  const [twitterExp, setTwitterExp] = useState(1)
+  const [facebookExp, setFacebookExp] = useState(1)
+  const [githubExp, setGithubExp] = useState(3)
   return (
     <div className="footer-container">
       <div className="footer-right">
         <div className="buttons">
-          <a href="https://www.linkedin.com/in/stephen-derbidge/" target="_blank">
+          <a href="https://www.linkedin.com/in/stephen-derbidge/" target="_blank" onClick={() => {
+            if (linkedinExp !== 0) {
+              addExpNotif(linkedinExp)
+              addExp(linkedinExp)
+              setLinkedInExp(0)
+            }
+          }}>
             <button className="social-button"><FaLinkedin /><span>LinkedIn</span></button>
           </a>
-          <a href="https://twitter.com/DerbidgeStephen/" target="_blank">
+          <a href="https://twitter.com/DerbidgeStephen/" target="_blank" onClick={() => {
+            if (twitterExp !== 0) {
+              addExpNotif(twitterExp)
+              addExp(twitterExp)
+              setTwitterExp(0)
+            }
+          }}>
             <button className="social-button"><FaTwitter /><span>Twitter</span></button>
           </a>
         </div>
         <div className="buttons">
-          <a href="https://www.facebook.com/profile.php?id=100004955105212" target="_blank">
+          <a href="https://www.facebook.com/profile.php?id=100004955105212" target="_blank" onClick={() => {
+            if (facebookExp !== 0) {
+              addExpNotif(facebookExp)
+              addExp(facebookExp)
+              setFacebookExp(0)
+            }
+          }}>
             <button className="social-button"><FaFacebook /><span>Facebook</span></button>
           </a>
-          <a href="https://www.github.com/sderbidge" target="_blank">
+          <a href="https://www.github.com/sderbidge" target="_blank" onClick={() => {
+            if (githubExp !== 0) {
+              addExpNotif(githubExp)
+              addExp(githubExp)
+              setGithubExp(0)
+            }
+          }}>
             <button className="social-button"><FaGithub /><span>Github</span></button>
           </a>
         </div>
